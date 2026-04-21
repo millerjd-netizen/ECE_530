402dcd0ec399beff0b1a6781c524aa7bd7b970ea
millerjd-netizen <millerjd@bu.edu>
2026-04-21 13:13:19 -0400

    Update annotation_service.py

--- EC530-Project2/services/annotation_service.py
+++ EC530-Project2/services/annotation_service.py
@@ -1,12 +1,32 @@
 from shared.events import subscribe_to, parse_message
 from shared.logger import get_logger
 
+
+##  this imports teh logging so the services can print the logs to make it easier to follow
+
+
 logger = get_logger(__name__)
 
 
+## this defines the main function that runs all of the services. Services are upload, infernece, annotaiton. 
+
+
 def main():
+
+# defines the main function
+    
     pubsub = subscribe_to("inference.completed")
+
+# this is a function taht connects to reddis and starts listening ofr messages on teh infenreces.completed channel
+
+    
+# this is using a function that is insode of the shared folder. 
+
+    
     logger.info("Annotation Service listening on inference.completed...")
+
+
+
 
     for message in pubsub.listen():
         try:
