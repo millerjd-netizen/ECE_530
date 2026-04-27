645c87786021d50a7a60f2a73a3263deba20a268
Jonathan Miller <millerjd@bu.edu>
2026-04-27 11:32:58 -0400

    Add subscribe and publish functionality to broker

--- Assignment6_PubSubMessagingSystem/src/broker.py
+++ Assignment6_PubSubMessagingSystem/src/broker.py
@@ -5,6 +5,20 @@ class Broker:
     def __init__(self):
         self.subscribers = defaultdict(list)
 
+    def subscribe(self, topic, handler):
+        self.subscribers[topic].append(handler)
+
+    def publish(self, event):
+        if event.topic not in self.subscribers:
+            return []
+
+        results = []
+        for handler in self.subscribers[event.topic]:
+            result = handler(event)
+            results.append(result)
+
+        return results
+
     def list_topics(self):
         return list(self.subscribers.keys())
 
